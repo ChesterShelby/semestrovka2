b02cc30e2254a81a65ee685e27633a1f434d8a7a
Vadim <NazarovvorazaN@mail.ru>
2021-12-20 15:12:47 +0300

    тестам положено начало

---
+++ test.py
@@ -0,0 +1,16 @@
+from pixel_battle import PixelBattle
+from server import Server
+
+from unittest import TestCase
+
+
+class TestGame(TestCase):
+
+    def setUpClass(cls) -> None:
+        pass
+
+    def setUp(self) -> None:
+        pass
+
+    def test_battle(self):
+        pass
